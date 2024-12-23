d5e57e4e1d2ff7ea0b8986f49a3748539f5b8c64
Edgar Nightingale <edgar.nightingale@btinternet.com>
2024-12-23 13:07:58 +0000

    Tweak message copy

--- src/services/battle.service.ts
+++ src/services/battle.service.ts
@@ -342,7 +342,7 @@ export async function action(skill: IBattleInput, session: Session & Partial<Ses
 			connection?.emit("message", {
 				color: "error.main",
 				username: user.username,
-				message: `${characterRecord.name} the ${characterRecord.characterClass.name} has been slain by ${enemyRecord.nameWithDeterminer} (kills: ${characterRecord.kills})`,
+				message: `${characterRecord.name} the ${characterRecord.characterClass.name} has been slain by ${enemyRecord.nameWithDeterminer} (${characterRecord.kills} kills)`,
 			});
 		}
 
@@ -355,7 +355,7 @@ export async function action(skill: IBattleInput, session: Session & Partial<Ses
 			connection?.emit("message", {
 				color: "text.primary",
 				username: user.username,
-				message: `${characterRecord.name} the ${characterRecord.characterClass.name} has defeated ${enemyRecord.nameWithDeterminer} (kills: ${characterRecord.kills})`,
+				message: `${characterRecord.name} the ${characterRecord.characterClass.name} has defeated ${enemyRecord.nameWithDeterminer} (${characterRecord.kills} kills)`,
 			});
 
 			if (battleRecord.level === FINAL_LEVEL) {
